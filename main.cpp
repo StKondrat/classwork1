d04ada8d70521a4defdda7718717878b1b737893
StKondrat <kndrt13@icloud.com>
2025-10-25 13:13:46 +0300

    Add func isPyth + fixes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <cstddef>
 
+bool isPyth(size_t a, size_t b, size_t c)
+{
+  bool p = (a*a == (b*b + c*c));
+  p = p || (b*b == (a*a + c*c));
+  p = p || (c*c == (a*a + b*b));
+  return p;
+}
+
 int main(){
   using u_t = unsigned;
-  u_t a = 0;
+  u_t a = 0, b = 0, c = 0;
   size_t count = 0;
+
   while (std::cin >> a)
   {
-    count += a * a;
+    count += isPyth(a, b, c) ? 1 : 0;
+    c = b;
+    b = a;
   }
 
   if (std::cin.eoF())
